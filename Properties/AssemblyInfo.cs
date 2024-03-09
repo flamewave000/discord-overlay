eaa145747ba3977126b318378a42cfcf0b14878c
Andrew Fryer <3588046+flamewave000@users.noreply.github.com>
2024-03-09 12:54:16 -0500

    v2.3.1 hotfix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DiscordOverlay")]
-[assembly: AssemblyCopyright("Copyright © flamewave000 2020")]
+[assembly: AssemblyCopyright("Copyright © flamewave000 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
 [assembly: NeutralResourcesLanguage("en-CA")]
